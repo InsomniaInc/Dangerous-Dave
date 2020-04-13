4c2c074cef83eea5f7c2e97ecb339386d2a00db9
Chaitanya Pathipati <chpathip@microsoft.com>
2020-04-13 17:08:33 +0530

    added physica material to player to prevent player from being stuck to walls

---
+++ Assets/scripts/BottomBarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomBarController : MonoBehaviour
+{
+    public SpriteRenderer GoThroughTheDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showGoThroughTheDoorMessage()
+    {
+        GoThroughTheDoor.color = new Color(GoThroughTheDoor.color.r, GoThroughTheDoor.color.g, GoThroughTheDoor.color.b,255);
+    }
+}
